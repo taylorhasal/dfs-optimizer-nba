e64fedf70cb0762f47110f6d33488b8189f4ad00
taylorhasal <e.taylor.hasal@gmail.com>
2018-12-22 19:35:29 -0700

    created grid display

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
           DAILY FANTASY SPORTS LINEUP OPTIMIZER NBA
         </header>
         <div className="Container">
-          <div className="Table">
-            <input></input>
-          </div>
+          <div className="Info">SELECT YOUR "LOCKED" PLAYERS FROM THE LIST OF AVAILABLE PLAYERS. CLICK THE "OPTIMIZE" BUTTON TO CREATE YOUR CUSTOM DFS LINEUP!</div>
+          <div className="Available-Players"></div>
+          <div className="Current-Roster"></div>
         </div>
       </div>
     );
